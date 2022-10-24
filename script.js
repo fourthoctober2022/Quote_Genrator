c8c2db7095079fcc1eab057db3109326340ffcb3
user <user@users-MBP.hitronhub.home>
2022-10-24 17:27:15 -0600

    update

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("hamneet welcome back");
+let apiQuotes=[];
+
+//Show New Quote
+function newQuote(){
+//pick a random quote  from apiQuotes array
+const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+console.log(quote);
+}
+
+
+//get Quotes from API
+async function getQuotes(){
+    const apiUrl="https://type.fit/api/quotes";
+    try{
+        const response = await fetch(apiUrl);
+        apiQuotes= await response.json();
+        // console.log(apiQuotes);
+        newQuote();
+    } catch(error){
+        //Catch Error Here
+    }
+}
+// On Load
+getQuotes();
+
+
+
+// //its for the local if you dont want to do through api
+// function newQuote(){
+//     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+//      console.log(quote);
+// }
+// newQuote();
